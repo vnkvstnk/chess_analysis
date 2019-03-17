db2153e00fe8850643a8f0a0cee5e555679e3183
Ivan Tiunov <tiunov.i@gubkin.ru>
2019-03-18 02:27:55 +0300

    No more rchess package. Errors on certain games in each (presumably) pgn chunk.

--- code/functions.R
+++ code/functions.R
@@ -13,28 +13,64 @@ square_to_index <- function (square) {
     c(i, j)
 }
 
+# Updates a table with another table
+update_table <- function (this, with_this) {
+    n <- intersect(names(this), names(with_this))
+    updated <- c(this[!names(this) %in% n], with_this[!names(with_this) %in% n], this[n] + with_this[n])
+    updated
+}
+
 
 # 1. What are the most popular squares for each piece?
 # (rchess package turned out to be extremely slow)
 popular_squares <- function (pgn, pieces = c("p", "k", "q", "b", "n", "r")) {
-    require(rchess)
+    require(stringr)
     require(dplyr)
     
-    # Importing a game
-    game <- Chess$new()
-    game$load_pgn(pgn)
-    data <- game$history(verbose = TRUE)  # data frame with information about each move
-    
     # Preallocating output variable
-    output <- vector("list", length(pieces)) %>% setNames(pieces)
+    output <- vector("list", length(pieces))
+    names(output) <- pieces
     for (i in names(output)) output[[i]] <- matrix(0, 8, 8)
     
-    # Populating the output
-    for (i in 1:nrow(data)) {
-        piece <- as.character(data[i, "piece"])
-        index <- square_to_index(as.character(data[i, "to"]))
-        output[[piece]][index[1], index[2]] <- output[[piece]][index[1], index[2]] + 1
-    }
-    output
+    # Split moves into white and black moves
+    # a. Split move string at digits with dots.
+    spl_pgn <- strsplit(pgn, "\\d+\\. ")[[1]][-1] %>%  # First element is empty
+        
+    # b. Squish each move (remove unneded spaces) and remove punctuation
+    str_squish() %>% str_remove_all("[!\\?\\+#]") %>%
+        
+    # c. Split at space
+    str_split(" ")
+    
+    moves <- list(white_moves = sapply(spl_pgn, function(x) x[1]),
+                  black_moves = sapply(spl_pgn, function(x) x[2]))
+    
+    # Create a list, that stores squares of each piece ****(Should account for castling in the future)****
+    squares <- list(pawn_squares=NULL, queen_squares=NULL, king_squares=NULL,
+                    bishop_squares=NULL, knight_squares=NULL, rook_squares=NULL)
+    
+    # a. Pawn moves
+    squares$pawn_squares <- moves$white_moves[grepl("^[a-h]\\d{1}$", moves$white_moves)]
+    
+    # b. Queen moves
+    squares$queen_squares <- moves$white_moves[grepl("^Q", moves$white_moves)]
+    
+    # c. King moves
+    squares$king_squares <- moves$white_moves[grepl("^K", moves$white_moves)]
+    
+    # d. Bishop moves
+    squares$bishop_squares <- moves$white_moves[grepl("^B", moves$white_moves)]
+    
+    # e. Knight moves
+    squares$knight_squares <- moves$white_moves[grepl("^N", moves$white_moves)]
+    
+    # f. Rook moves
+    squares$rook_squares <- moves$white_moves[grepl("^R", moves$white_moves)]
+    
+    # Take only last two characters and present each set of squares as a table
+    squares <- sapply(squares, str_sub, -2, -1)
+    squares <- sapply(squares, table)
+    
+    squares
 }
 
--- code/process_data.R
+++ code/process_data.R
@@ -5,34 +5,45 @@ library(tidyr)
 source("./code/functions.R")
 
 # Load pgn files (for now, I will use only one file with 97339 games)
-games <- read.pgn("./data/raw/2018-05.1.pgn",
-                  n.moves = TRUE,
-                  extract.moves = FALSE, 
-                  stat.moves = FALSE,
-                  add.tags = c("WhiteElo", "BlackElo","ECO", "WhiteTitle", "BlackTitle",
-                               "TimeControl", "Termination")) %>%
-    as_tibble() %>%
-    mutate_at(c("Termination", "ECO", "WhiteTitle", "BlackTitle"),
-              list(factor)) %>%
-    mutate(Date = as.Date(Date, "%Y.%m.%d"),
-           Tournament = grepl("tournament",
-                              Event,
-                              ignore.case = TRUE),
-           Type = factor(sapply(strsplit(Event, " "), function(x) x[2]))) %>%
-    separate(col = TimeControl,
-             into = c("BaseTime", "Increment"),
-             convert = TRUE) %>%
-    select(-c(Round, Event)) %>%
-    filter(NMoves >= 3)
+rds_file <- "./data/processed/2018-05.4.rds"
+pgn_file <- "./data/raw/2018-05.4.pgn"
 
-
-pieces <- c("p", "k", "q", "b", "n", "r")
-popularity <- vector("list", length(pieces)) %>% setNames(pieces)
-for (i in names(popularity)) popularity[[i]] <- matrix(0, 8, 8)
-
-subset <- games[1:300, ]
-
-for (i in 1:nrow(subset)) {
-    output <- popular_squares(subset[i, ]$Movetext)
-    popularity <- Map("+", popularity, output)
+if (!file.exists(rds_file)) {
+    games <- read.pgn(pgn_file,
+                      n.moves = TRUE,
+                      extract.moves = FALSE, 
+                      stat.moves = FALSE,
+                      add.tags = c("WhiteElo", "BlackElo","ECO", "WhiteTitle", "BlackTitle",
+                                   "TimeControl", "Termination")) %>%
+        as_tibble() %>%
+        mutate_at(c("Termination", "ECO", "WhiteTitle", "BlackTitle"),
+                  list(factor)) %>%
+        mutate(Date = as.Date(Date, "%Y.%m.%d"),
+               Tournament = grepl("tournament",
+                                  Event,
+                                  ignore.case = TRUE),
+               Type = factor(sapply(strsplit(Event, " "), function(x) x[2]))) %>%
+        separate(col = TimeControl,
+                 into = c("BaseTime", "Increment"),
+                 convert = TRUE) %>%
+        select(-c(Round, Event)) %>%
+        filter(NMoves >= 3)
+    saveRDS(games, rds_file)
+} else {
+    games <- readRDS(rds_file)
 }
+
+# Now let's try to get all the squares from all the games
+
+squares <- list(pawn_squares=NULL, queen_squares=NULL, king_squares=NULL,
+                           bishop_squares=NULL, knight_squares=NULL, rook_squares=NULL)
+# profvis::profvis({
+for (i in 1:nrow(games)) {
+    # if (i == 36931) next
+    game_squares <- popular_squares(games$Movetext[i])
+    for (field in names(squares)) {
+        squares[[field]] <- update_table(squares[[field]], game_squares[[field]])
+    }
+    if (i %% 1000 == 0) print(i)
+}
+# })
