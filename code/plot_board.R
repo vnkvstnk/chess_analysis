88a600a6a121f94c65a4bd0550a63d8109987bb5
Ivan Tiunov <tiunov.i@gubkin.ru>
2019-04-11 17:49:00 +0300

    Plotting function modified.

--- code/plot_board.R
+++ code/plot_board.R
@@ -2,6 +2,7 @@ plot_board <- function(board) {
     require(ggplot2)
     require(tidyr)
     require(dplyr)
+    require(RColorBrewer)
     # Reshaping "board" to make it suitable for plotting
     board <- as_tibble(board) %>%           # convert to dataframe
         mutate(rank = rownames(board)) %>%  # adding rank numbers as separate variable
@@ -12,7 +13,20 @@ plot_board <- function(board) {
                 aes(x = file,
                     y = rank,
                     fill = count)) + 
-        geom_tile()
+        geom_tile(colour="white",
+                  size=0.25) + 
+        coord_fixed() + 
+        labs(x = "",
+             y = "",
+             fill = "Counts") + 
+        theme(text = element_text(size = 22,
+                                  face = "bold"),
+              axis.ticks = element_blank(),
+              legend.text = element_text(size = 12,
+                                         face = "plain"),
+              legend.title = element_text(size = 12,
+                                          face = "plain"),
+              panel.background = element_blank())
     
     print(g)
 
